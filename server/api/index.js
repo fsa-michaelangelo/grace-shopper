8c62d62d8c3339567aa9ffab6d6809ac85390d10
Johnny OMara <omarajohnj@gmail.com>
2020-08-05 09:51:43 -0500

    single bread backend working

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/items', require('./items'))
+router.use('/breads', require('./breads'))
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
