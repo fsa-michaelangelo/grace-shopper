770eb1ea4b6eaeb679f4a6afe699fd9af0012600
Zach Bryce <zachbryce11@gmail.com>
2020-08-05 14:35:52 -0500

    init commit

--- client/components/index.js
+++ client/components/index.js
@@ -4,5 +4,7 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
+export {default as Breads} from './Breads'
 export {default as UserHome} from './user-home'
+export {default as home} from './home'
 export {Login, Signup} from './auth-form'
