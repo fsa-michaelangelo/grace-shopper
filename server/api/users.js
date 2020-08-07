ebc5ec101228a26e9a71502f0bb4d839b2484cfb
Maddie Higgins <maddiehiggins9@gmail.com>
2020-08-07 13:43:28 -0500

    added admin middleware to users api

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,18 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
-const {Order} = require('../db/models')
+//const {Order} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+export const adminsOnly = (req, res, next) => {
+  if (!req.user.isAdmin) {
+    const err = new Error(`You aren't authorized to access this.`)
+    err.status = 401
+    next(err)
+  }
+  next()
+}
+
+router.get('/', adminsOnly, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // even though users' passwords are encrypted, it won't help if we just
